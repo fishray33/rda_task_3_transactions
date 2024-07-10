4e32880cbf11588fb1ee4172d5607691584f1046
Ruslan CHavus <chruslan4@gmail.com>
2024-07-10 14:48:00 +0300

    Solution 3

--- task.sql
+++ task.sql
@@ -1,12 +1,10 @@
 USE ShopDB; 
 
 INSERT INTO Orders (ID, CustomerID, Date) VALUES (1, 1, '2024-07-09');
-INSERT OrderItems (ID, OrderID, ProductID, Count) VALUES (1, 1, 1, 0);
 
 START TRANSACTION; 
 
-UPDATE Products SET WarehouseAmount = WarehouseAmount - 1 WHERE Name = "AwersomeProduct";
-
-UPDATE OrderItems SET Count = 1 WHERE ID = 1;
+INSERT INTO OrderItems (ID, OrderID, ProductID, Count) VALUES (1, 1, 1, 1);
+UPDATE Products SET WarehouseAmount = (WarehouseAmount - 1) WHERE Name = "AwersomeProduct";
 
 COMMIT; 
